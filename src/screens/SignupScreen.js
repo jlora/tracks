f74a813be9512181b03e9b7888acaba32a4f5efb
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2020-11-25 21:15:26 -0700

    created Spacer component and styling SignupScreen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
-  return <> 
-    <Text style={{ fontSize: 48}}>SignUpScreen</Text>
-    <Button 
-      title='Go to SignIn' 
-      onPress={() => navigation.navigate('Signin')}
-    />
-    <Button 
-      title='Go to main Flow' 
-      onPress={() => navigation.navigate('mainFlow')}
-    />
-  </>
+  return <View style={styles.container}>
+    <Spacer>
+      <Text h3>Sign Up for Tracker</Text>
+    </Spacer>
+    <Input label='Email' />
+    <Input label='Password' />
+    <Spacer>
+      <Button title='Sign Up' />
+    </Spacer>
+  </View>
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 200
+  }
+});
 
 export default SignupScreen;
